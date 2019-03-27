3ac7b16f0908956fa33a22153262619e6389ed90
Maksym Sditanov <msditanov@200volts.com>
2019-03-27 14:54:02 +0200

    Ignore build dir
    :Release Notes:
    
    :Detailed Notes:
    
    :Testing Performed:
    No tests
    
    :QA Notes:
    
    :Issues Addressed:
    
    Signed-off-by: Maksym Sditanov <msditanov@200volts.com>

--- cmake/Application.cmake
+++
@@ -1,9 +0,0 @@
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Modules/")
-
-# include only win32 specific stuff on win32 platfrom
-if (WIN32)
-	set(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} "${CMAKE_SOURCE_DIR}/cmake/Win32_64_Specific/")
-endif()
-
-include("${CMAKE_SOURCE_DIR}/cmake/CompilerFlags.cmake")
